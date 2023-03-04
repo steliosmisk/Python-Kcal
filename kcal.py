e13f717a06fe892003bc5e76d75eef79ca322f92
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-03-04 08:51:50 +0200

    Update kcal.py

--- kcal.py
+++ kcal.py
@@ -4,20 +4,19 @@ print("[2] FEMALE")
 print("[3] EXIT")
 
 user_choice = input("Enter your choice: ")
-while user_choice in ['1', '2'] and user_choice != '3':
+
+while user_choice in ['1', '2']:
     try:
         weight = int(input("[KG] Enter your weight: "))
         height = int(input("[CM] Enter your height: "))
         age = int(input("[AGE] Enter your age: "))
-        active = input(
-            "[?] Enter your activity (Sedentary, Lightly, Moderately, Active, VeryActive): ").lower()
+        active = input("[?] Enter your activity (Sedentary, Lightly, Moderately, Active, VeryActive): ").lower()
 
         if user_choice == '1':
-            mbr_results = 66.47 + (13.75 * weight) + \
-                (5.003 * height) - (6.755 * age)
+            mbr_results = 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age)
         else:
-            mbr_results = 655.1 + (9.563 * weight) + \
-                (1.850 * height) - (4.676 * age)
+            mbr_results = 655.1 + (9.563 * weight) + (1.850 * height) - (4.676 * age)
+
         if active == 'sedentary':
             kcal_results = mbr_results * 1.2
         elif active == 'lightly':
@@ -28,6 +27,7 @@ while user_choice in ['1', '2'] and user_choice != '3':
             kcal_results = mbr_results * 1.725
         else:
             kcal_results = mbr_results * 1.9
+
         print("[KCAL] Daily Calories based on your info:", kcal_results)
         break
     except ValueError:
